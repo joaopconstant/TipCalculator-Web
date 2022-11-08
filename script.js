bdd553e254d5bcaab9c39909a25bf8ac734615b5
João Pedro <110848773+joaopconstant@users.noreply.github.com>
2022-11-07 22:20:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculateTip(event) {
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let serviceQuality = document.getElementById('serviceQuality').value;
+    let numPeople = document.getElementById('people').value;
+
+    if(bill == '' | serviceQuality == 0) {
+        alert("Por favor, preencha os valores!")
+        return;
+    }
+
+    if(numPeople == '' | numPeople <= 1) {
+        numPeople = 1;
+        document.getElementById('each').style.display = 'none'
+    }   else {
+        document.getElementById('each').style.display = 'block'
+    }
+
+    let total = (bill * serviceQuality) / numPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = 'block';
+
+}
+
+document.getElementById('totalTip').style.display = 'none';
+document.getElementById('each').style.display = 'none';
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
